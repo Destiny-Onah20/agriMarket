45fb3090c6a9011b475d49ced20edd8bc553967f
Destiny-Onah20 <onahdestinyc20@gmail.com>
2023-02-13 11:19:12 +0100

    little changes

--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -8,7 +8,7 @@ const productRoutes = express.Router();
 productRoutes.route("/product").get(allProducts);
 productRoutes.route("/product/cat").get(categoriesForPoultry);
 productRoutes.route("/product/fish").get(categoriesForFishery);
-productRoutes.route("/product/:userId/:productId").get(singleProduct);
+productRoutes.route("/product/:productId").get(singleProduct);
 productRoutes.route("/product/:userId").post(realAdmin  , postProduct);
 productRoutes.route("/updProduct/:userId/:productId").patch(realAdmin , updtProduct);
 productRoutes.route("/delProduct/:userId/:productId").delete(realAdmin , delProducts);
