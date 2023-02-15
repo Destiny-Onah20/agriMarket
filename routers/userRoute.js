8fc5fd875fd85fff0ddf8d84c501b3513ac98d0a
Destiny-Onah20 <onahdestinyc20@gmail.com>
2023-02-15 17:15:25 +0100

    based

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -9,7 +9,7 @@ userRoute.route("/admin").post(adminSignUp);
 userRoute.route("/login").post(logIn);
 userRoute.route("/verify/:id").post(verifyU);
 userRoute.route("/forgot").post(forgotPassWrd);
-userRoute.route("/changePaswrd/:userId/:token").post(changePassWrd);
+userRoute.route("/changePaswrd/:userId").post(changePassWrd);
 userRoute.route("/updAdmin/:userId").patch(updateAdmin)
 userRoute.route("/delAdmin/:userId").delete(delAdmin)
 
