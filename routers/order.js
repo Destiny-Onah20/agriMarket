13188d2ac4a6b598f750c429b385dc3c7df0a753
Destiny-Onah20 <onahdestinyc20@gmail.com>
2023-02-16 12:03:26 +0100

    image

--- routers/order.js
+++ routers/order.js
@@ -6,8 +6,8 @@ const { isSuperAdmin, realAdmin } = require("../helpers/authentic");
 const orderRoute = express.Router();
 
 orderRoute.route("/order").post(placeOrder);
-orderRoute.route("/allorder/:userId").get(realAdmin ,allOrders);
-orderRoute.route("/singleorder/:userId").get(isSuperAdmin ,singleOrder);
+orderRoute.route("/allorder/:userId").get(isSuperAdmin ,allOrders);
+orderRoute.route("/singleorder/:userId/:orderId").get(isSuperAdmin ,singleOrder);
 orderRoute.route("/delevered/:userId").post(deliver);
 
 module.exports = orderRoute
