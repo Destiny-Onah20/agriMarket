d4893d889c92d4f89c198145d726950e26f312c2
Destiny-Onah20 <onahdestinyc20@gmail.com>
2023-02-17 01:51:13 +0100

    still changing

--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -9,7 +9,7 @@ productRoutes.route("/product").get(allProducts);
 productRoutes.route("/product/cat").get(categoriesForPoultry);
 productRoutes.route("/product/fish").get(categoriesForFishery);
 productRoutes.route("/product/:productId").get(singleProduct);
-productRoutes.route("/product/:userId").post(realAdmin  , postProduct);
+productRoutes.route("/product/:userId").post( postProduct);
 productRoutes.route("/updProduct/:userId/:productId").patch(realAdmin , updtProduct);
 productRoutes.route("/delProduct/:userId/:productId").delete(realAdmin , delProducts);
 
